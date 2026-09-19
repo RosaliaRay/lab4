d8723eb15d651edc2851330641542d9ae62a85c1
Rosalia <192719739+RosaliaRay@users.noreply.github.com>
2026-09-19 11:30:04 +0300

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,47 @@
+/*
+    Студент: Мареева Ангелина Ильинична
+    Группа: ПИ 11
+    Назначение: Консольный калькулятор
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    double a, b, result;
+    int command;
+
+    printf("Введите два числа: ");
+    scanf("%lf %lf", &a, &b);
+
+    printf("1 - сложение\n2 - вычитание\n3 - умножение\n4 - деление\n");
+    printf("Выберите операцию: ");
+    scanf("%d", &command);
+
+    switch (command) {
+        case 1:
+            result = a + b;
+            printf("Результат: %.2f\n", result);
+            break;
+        case 2:
+            result = a - b;
+            printf("Результат: %.2f\n", result);
+            break;
+        case 3:
+            result = a * b;
+            printf("Результат: %.2f\n", result);
+            break;
+        case 4:
+            if (b != 0) {
+                result = a / b;
+                printf("Результат: %.2f\n", result);
+            } else {
+                printf("Ошибка: деление на ноль.\n");
+            }
+            break;
+        default:
+            printf("Ошибка: неизвестная операция.\n");
+            break;
+    }
+
+    return 0;
+}
